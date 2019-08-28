90834496f2e423d1e6ab1735268f3c8b106c9a04
leowyh <leowyh4@gmail.com>
2019-08-28 13:25:36 +0800

    Level 2 Add,List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,27 +1,66 @@
 import java.util.*;
 
 public class Duke {
+    public static final String line = "    ____________________________________________________________\n";
+    public static final ArrayList<Task> mylist = new ArrayList<Task>();
+
     public static void main(String[] args) {
-        String line = "    ____________________________________________________________\n";
-        String logo = line
-                    + "     Hello! I'm Duke\n"
-                    + "     What can I do for you?\n"
-                    + line
-                    + "\n";
-        System.out.println(logo);
+
+
+        StartMessage();
         Scanner in = new Scanner(System.in);
         while(true){
-            String word = in.next();
-            if (word.equals("bye")) {
-                String output = line + "     Bye. Hope to see you again soon!\n" + line;
-                System.out.println(output);
+            String input = in.nextLine();
+            if (input.equals("bye")) {
+                //print bye message
+                ByeMessage();
                 in.close();
                 break;
-            }else{
-                String output = line + "     " + word + "\n" + line;
-                System.out.println(output);
+            }else if (input.equals("list")){
+                //print out current list
+                PrintList();
+            }
+            else{
+                //add task to list
+                AddInput(input);
             }
         }
 
+    }
+
+    private static void PrintList(){
+        int listsize = mylist.size();
+        System.out.print(line);
+        for (int i = 0; i < listsize; i++){
+            int listnum = i+1;
+            System.out.print("     " + listnum + ". " + mylist.get(i).GiveTask() + "\n");
+        }
+        System.out.print(line);
+    }
+
+    private static void AddInput(String input){
+        Task tasktoadd = new Task(input);
+        mylist.add(tasktoadd);
+        AddedTaskMessage(input);
+    }
+
+    private static void AddedTaskMessage(String input){
+        System.out.print(line + "     added: " + input);
+        System.out.println();
+        System.out.print(line);
+    }
+
+    private static void StartMessage(){
+        String logo = line
+                + "     Hello! I'm Duke\n"
+                + "     What can I do for you?\n"
+                + line
+                + "\n";
+        System.out.println(logo);
+    }
+
+    private static void ByeMessage(){
+        String output = line + "     Bye. Hope to see you again soon!\n" + line;
+        System.out.println(output);
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,11 @@
+public class Task {
+    private String mytask;
+
+    public Task(String input){
+        this.mytask = input;
+    }
+    public String GiveTask(){
+        return this.mytask;
+    }
+
+}
